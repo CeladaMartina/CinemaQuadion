24a604dda3c787993e7408306bc79ea482086287
Martina <mcelada@quadiontech.com>
2025-02-18 12:09:10 -0300

    se desarrollo parte de activity

--- src/Components/Body Section/Activity Section/Activity.jsx
+++ src/Components/Body Section/Activity Section/Activity.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
 import './activity.css'
+import { BsArrowRightShort } from "react-icons/bs";
 
 const Activity = () => {
   return (
-    <div>Activity</div>
+    <div className='activitySection'>
+      <div className="heading flex">
+        <h1>Resent Activity</h1>
+        <button className='btn flex'>
+          See All <BsArrowRightShort className='icon' />
+        </button>
+      </div>
+
+      <div className="secContainer grid">
+        <div className="singleCustomer flex">
+          <img  src='https://us.123rf.com/450wm/deagreez/deagreez2208/deagreez220800731/189858586-photo-of-pretty-overjoyed-girl-toothy-smile-show-tongue-out-wear-red-white-stripes-t-shirt-isolated.jpg?ver=6' alt='Image Name'/>
+          <div className="customerDetails">
+            <span className='name'>Ola Martha</span>
+            <small>Ordered a new plant</small>
+          </div>
+          <div className="duration">
+            2 min ago
+          </div>
+        </div>
+        <div className="singleCustomer flex">
+          <img  src='https://us.123rf.com/450wm/deagreez/deagreez2208/deagreez220800731/189858586-photo-of-pretty-overjoyed-girl-toothy-smile-show-tongue-out-wear-red-white-stripes-t-shirt-isolated.jpg?ver=6' alt='Image Name'/>
+          <div className="customerDetails">
+            <span className='name'>Ola Martha</span>
+            <small>Ordered a new plant</small>
+          </div>
+          <div className="duration">
+            2 min ago
+          </div>
+        </div>
+        <div className="singleCustomer flex">
+          <img  src='https://us.123rf.com/450wm/deagreez/deagreez2208/deagreez220800731/189858586-photo-of-pretty-overjoyed-girl-toothy-smile-show-tongue-out-wear-red-white-stripes-t-shirt-isolated.jpg?ver=6' alt='Image Name'/>
+          <div className="customerDetails">
+            <span className='name'>Ola Martha</span>
+            <small>Ordered a new plant</small>
+          </div>
+          <div className="duration">
+            2 min ago
+          </div>
+        </div>
+        <div className="singleCustomer flex">
+          <img  src='https://us.123rf.com/450wm/deagreez/deagreez2208/deagreez220800731/189858586-photo-of-pretty-overjoyed-girl-toothy-smile-show-tongue-out-wear-red-white-stripes-t-shirt-isolated.jpg?ver=6' alt='Image Name'/>
+          <div className="customerDetails">
+            <span className='name'>Ola Martha</span>
+            <small>Ordered a new plant</small>
+          </div>
+          <div className="duration">
+            2 min ago
+          </div>
+        </div>
+        <div className="singleCustomer flex">
+          <img  src='https://us.123rf.com/450wm/deagreez/deagreez2208/deagreez220800731/189858586-photo-of-pretty-overjoyed-girl-toothy-smile-show-tongue-out-wear-red-white-stripes-t-shirt-isolated.jpg?ver=6' alt='Image Name'/>
+          <div className="customerDetails">
+            <span className='name'>Ola Martha</span>
+            <small>Ordered a new plant</small>
+          </div>
+          <div className="duration">
+            2 min ago
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
